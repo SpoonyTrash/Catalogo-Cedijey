0845191384d5089e48c00b215e542b757b2492af
Spoony <106294172+SpoonyTrash@users.noreply.github.com>
2026-08-31 18:52:46 -0600

    feat: allow catalog covers from Cloudinary

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/m1hqc5v/image/upload/**",
+                search: "",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
